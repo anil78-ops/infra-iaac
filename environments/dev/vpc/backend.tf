7316fc2cbd3d361cf86b7813dd03e3dec8fa4a63
anil78-ops <vanilkumar4191@gmail.com>
2026-03-08 15:56:46 +0530

    Fix formatting in backend.tf

--- environments/dev/vpc/backend.tf
+++ environments/dev/vpc/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "iaac-dev-anil"
-    key     = "terraform/services/vpc/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+    bucket       = "iaac-dev-anil"
+    key          = "terraform/services/vpc/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
     use_lockfile = true
   }
 }
