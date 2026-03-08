9ce6c62372a9b4406b21ea7a723885f38d0d39e5
anil78-ops <vanilkumar4191@gmail.com>
2026-03-08 15:34:52 +0530

    Update backend.tf

--- environments/dev/vpc/backend.tf
+++ environments/dev/vpc/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "iaac-dev"
+    bucket  = "iaac-dev-anil"
     key     = "terraform/services/vpc/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
