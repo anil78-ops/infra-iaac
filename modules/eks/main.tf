eb006cf7b5a739e139b6ed598a92b4133eaf3fba
anil78-ops <vanilkumar4191@gmail.com>
2026-03-09 19:31:08 +0530

    Update EKS scaling configuration for instance sizes

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -109,9 +109,9 @@ resource "aws_eks_node_group" "default" {
   instance_types = [var.instance_type]
 
   scaling_config {
-    desired_size = 1
-    min_size     = 1
-    max_size     = 1
+    desired_size = 2
+    min_size     = 2
+    max_size     = 3
   }
 
   disk_size = 20
